01edaebf5b8245a2f2a5f97dbcb3e47adbf3f5d6
Justin Turner <jmturner638@gmail.com>
2021-07-30 22:56:54 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    count = int(input("How many times?"))
+
+    for i in range(count):
+        print_text()
+
+def print_text():
+    print("In a hole in the ground there lived a method")
 
 if __name__ == '__main__':
     main()
